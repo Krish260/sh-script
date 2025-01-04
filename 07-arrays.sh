65739b930e00cfb67c0ad2908a607b933adb0f8c
Krish260 <krishnamurthyv07@gmail.com>
2025-01-04 16:13:39 +0530

    variable added

--- 07-arrays.sh
+++ 07-arrays.sh
@@ -2,8 +2,8 @@
 
 Movies=("devara" "pushpa" "rrr")
 
-echo "the first movie is: ${Movies[1]}"
-echo "the second movie is: ${Movies[2]}"
-echo "the third movie is: ${Movies[3]}"
+echo "the first movie is: ${Movies[0]}"
+echo "the second movie is: ${Movies[1]}"
+echo "the third movie is: ${Movies[2]}"
 
 echo "all the movies are: ${Movies[@]}"
