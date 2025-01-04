14cdb79b50b98150363c4a771ab11cafce4f6dd3
Krish260 <krishnamurthyv07@gmail.com>
2025-01-04 16:10:05 +0530

    variable added

--- 06-datatypes.sh
+++ 06-datatypes.sh
@@ -6,5 +6,5 @@ number2=$2
 timestamp=$(date)
 echo "script executed at:$timestamp"
 sum=$((number1+number2))
-echo "sum of number1 and number2 ia:$sum"
+echo "sum of number1 and number2 is:$sum"
 
---
+++ 07-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Movies=("devara" "pushpa" "rrr")
+
+echo "the first movie is: ${Movies[1]}"
+echo "the second movie is: ${Movies[2]}"
+echo "the third movie is: ${Movies[3]}"
+
+echo "all the movies are: ${Movies[@]}
