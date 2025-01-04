a0aa9e8eb1d5903e2aa20c1b8a5cddad4e90e950
Krish260 <krishnamurthyv07@gmail.com>
2025-01-04 15:25:43 +0530

    variable added

--- 03-variables.sh
+++ 03-variables.sh
@@ -3,7 +3,7 @@
 person1=rajjo
 person2=kittu
 
-echo "$person1:hi $person2"
-echo "${person2}:hello $person1"
-echo "$person1:how r u" 
-echo "$person2:great doing"
+echo "$person1::hi $person2"
+echo "${person2}::hello $person1"
+echo "$person1::how r u" 
+echo "$person2::great doing"
